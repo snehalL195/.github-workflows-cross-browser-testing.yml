81aeced5b035cc957ba78e77bcff311167534f26
snehalL195 <nashtech@INL-1677PG3>
2024-12-26 22:04:22 +0530

    committedcode

---
+++ abc/src/test/java/abc/basetest.java
@@ -0,0 +1,34 @@
+package abc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class basetest {
+
+    protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+
+    @BeforeMethod
+    @Parameters("browser")
+    public void setup(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browser); 
+        driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities)); 
+    }
+
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        getDriver().quit();
+        driver.remove();
+    }
+}
---
+++ abc/src/test/java/abc/firsttest.java
@@ -0,0 +1,12 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class firsttest extends basetest {
+
+    @Test
+    public void testGoogle() {
+        getDriver().get("https://www.google.com"); 
+        // Add your test logic here
+    }
+}
